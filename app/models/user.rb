2ef4831314dabaee73443cfc67550cb6c9944596
Charles <charleschoi87@gmail.com>
2013-11-22 15:30:01 +0900

    added user model has_ancestry

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-  attr_accessible :banner, :color, :facebook_uid, :ip, :point, :username
+  attr_accessible :banner, :color, :facebook_uid, :ip, :point, :username, :ancestry
+	has_ancestry
 end
