06795aac69bad6a097f07ca41a924937b7f43a6b
Charles <charleschoi87@gmail.com>
2013-11-22 14:55:33 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iamyourfather::Application.config.secret_token = '7b995710fdcb58f0192ed009e23fb0ba5e948a4e603bd5ef54659e72c38d4db15ee7a1d634abd2910172cde6c36127ab418f6aa0ca0ce8634334d5085472a92e'
