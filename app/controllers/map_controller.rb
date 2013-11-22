4b2db35864fdf7ea433aed791d59ad148f08d833
Charles <charleschoi87@gmail.com>
2013-11-22 16:20:25 +0900

    hi

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,7 +1,19 @@
 class MapController < ApplicationController
   def index
 		@name = params[:id]
+		# ipcheck
+		# cookie check
+		# facebook login check
+		#
+		# return gexf
   end
+
+	def data
+		data = { :sample => "test" }
+		respond_to do |format|
+			format.gexf { render :gexf => data }
+		end
+	end
 
   def seize
   end
