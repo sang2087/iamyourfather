de2a27a7f4285b357266e1fb0deef9f9fdf3578f
hogoo <sang2087@gmail.com>
2013-11-26 21:10:47 +0900

    change i18n

---
+++ app/workers/invi_worker.rb
@@ -0,0 +1,30 @@
+#encoding : utf-8
+class InviWorker
+	URL = "http://i-am-your-father.com/"
+
+  APP_ID = '215886731905255'
+  APP_SECRET =  '96c1ecb73693ad1ce0f6d0c754450c75'
+  include Sidekiq::Worker
+
+  def perform(user_id, from_uid, to_uid, from_oauth_token, locale)
+    id = "-#{from_uid}@chat.facebook.com"
+    to = "-#{to_uid}@chat.facebook.com"
+		logger.info "SEND message to #{to}"
+		puts "SEND message to #{to}"
+
+		if locale == 'ko'
+			body = "당신을 우리 가족에 초대합니다. 아래 링크를 참고하세요!\n#{URL}#{user_id}"
+		else
+			body = "I wanna make family. Come here.\n#{URL}#{user_id}"
+		end
+		send_message = Jabber::Message.new to, body
+
+		client = Jabber::Client.new Jabber::JID.new(id)
+		client.connect
+		client.auth_sasl(Jabber::SASL::XFacebookPlatform.new(client, APP_ID, from_oauth_token, APP_SECRET), nil)
+		client.send send_message
+		client.close
+
+		logger.info "SENDED message to #{to}"
+  end
+end
---
+++ db/temp_seed/seeds3.rb
@@ -0,0 +1,91 @@
+puts "start seed"
+puts "base data start"
+BaseData.create(:mytype => "PointLog",
+								:code => 1,
+								:point => 5,
+								:description => "no facebook link in")
+
+BaseData.create(:mytype => "PointLog",
+								:code => 2,
+								:point => 30,
+								:description => "facebook link in")
+
+BaseData.create(:mytype => "PointLog",
+								:code => 3,
+								:point => -300,
+								:node_cnt => 15,
+								:description => "independance")
+
+BaseData.create(:mytype => "PointLog",
+								:code => 4,
+								:point => -10, #mulify your node_cnt
+								:node_cnt => 5,
+								:description => "betray")
+
+BaseData.create(:mytype => "PointLog",
+								:code => 5,
+								:point => -50, #mulify your node_cnt
+								:node_cnt => 30,
+								:description => "seize")
+
+BaseData.create(:mytype => "PointLog",
+								:code => 6,
+								:point => 5,
+								:description => "invitation message")
+
+BaseData.create(:mytype => "PointLog",
+								:code => 7,
+								:point => 10,
+								:description => "login 10 min-term")
+
+BaseData.create(:mytype => "PointLog",
+								:code => 8,
+								:point => 50,
+								:description => "Share Feed")
+puts "base data end"
+
+ROOT_SIZE = 1
+SIZE = 1
+User.create(:ip => "0.0.0.0",
+						:username => "The God",
+						:point => 0,
+						:color => "#{100 + rand(156)}/#{100 + rand(156)}/#{100 + rand(156)}",
+						:banner => "I am the God",
+						:node_cnt => 0)
+
+=begin
+end
+1.upto(SIZE) do |i|
+	User.create(:ip => "0.0.0.0",
+							:username => "man #{i}",
+							:point => 0,
+							:banner => "#{i} node!!",
+							:node_cnt => 1 )
+end
+
+(ROOT_SIZE + 1).upto(ROOT_SIZE + SIZE) do |i|
+	r = rand(i) + 1
+	user = User.find(i)
+	if(i != r)
+		user.parent = User.find(r)
+		user.save!
+		user.color = user.root.color
+		user.save!
+	else
+		user.parent = User.find(rand(ROOT_SIZE) + 1)
+		user.save!
+		user.color = user.root.color
+		user.save!
+	end
+end
+
+puts "counting..."
+User.all.each do |u|
+	u.node_cnt = u.descendant_ids.count + 1
+	u.save!
+end
+
+=end
+#User.all_tree_set_xy
+puts "That's all!"
+
