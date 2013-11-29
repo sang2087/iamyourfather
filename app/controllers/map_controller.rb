926d63d74d2e970e9765d9ea927195319cce0f3f
hogoo <sang2087@gmail.com>
2013-11-29 20:12:28 +0900

    rank without pagination

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -59,6 +59,10 @@ class MapController < ApplicationController
 		puts "@is_point_get_login #{@is_point_get_login}"
 		session[:login_point] = false
 		@login_point = BaseData.where(:mytype=>"PointLog", :code=>7).first.point
+		@node_cnt_rank = @user.my_rank "node_cnt"
+		@point_rank = @user.my_rank "point"
+		@all_rank_node_cnt = User.all_rank "node_cnt"
+		@all_rank_point = User.all_rank "point"
 
   end
 
